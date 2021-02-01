489ed00324032715184d0ebe76b020e20c2d043e
Hansol Jang <kjhs0320@gmail.com>
2021-02-01 12:12:10 +0900

    day 3. Date Picker 주석 추가

--- DatePiker/DatePiker/ViewController.swift
+++ DatePiker/DatePiker/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // 타이머가 구동되면 실행되는 함수
     let timeSelector: Selector = #selector(ViewController.updateTime)
-    let interval = 1.0
-    var count = 0
+    let interval = 1.0  // 간격 1초
+    var count = 0       // 타이머가 설정한 간격대로 실행되는 지 확인하기 위한 테스트용 변수
     
-    @IBOutlet var CurrentTime: UILabel!
-    @IBOutlet var PikerTime: UILabel!
+    @IBOutlet var CurrentTime: UILabel!  // 현재 시간 레이블 아울렛 변수
+    @IBOutlet var PikerTime: UILabel!    // 선택 시간 레이블 아울렛 변수
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,19 +26,22 @@ class ViewController: UIViewController {
 
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
-        let datePickerView = sender
+        let datePickerView = sender  // 데이터피커로 선택한 인수를 저장
         
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        let formatter = DateFormatter() //DateFormatter 클래스 상수 선언
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE" // formatter의 dateFormat 속성 설정
         PikerTime.text = "선택시간:" + formatter.string(from: datePickerView.date)
+        // 데이트피커에서 선택한 날짜를 formatter의 dateFormat에서 설정한 포맷대로
+        // strintg 메서드를 사용하여 string으로 변환
         
     }
     @objc func updateTime() {
-//        제대로 동작하는지 테스트
-//        CurrentTime.text = String(count)
-//        count = count + 1
+        // 해당 함수가 제대로 동작하는지 테스트
+        // count값을 문자열로 바꿔서 CurrentTime.text로 출력
+        // CurrentTime.text = String(count)
+        // count = count + 1
         
-        let date = NSDate()
+        let date = NSDate() // 현재 시간을 가져옴
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss eee"
