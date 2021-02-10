ef18e07af90ed1be09b0159dc81a5215d88fd45e
Hansol Jang <kjhs0320@gmail.com>
2021-02-10 23:55:05 +0900

    day 12. Table View start

---
+++ Table/Table/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Table
+//
+//  Created by 장한솔 on 2021/02/10.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+    @IBOutlet var Item: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // ç
+ 
+ 
+ 
+ 
