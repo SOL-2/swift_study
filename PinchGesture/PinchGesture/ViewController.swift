c39f2dbd911242192828f4b5f13d26e9d28e678f
Hansol Jang <kjhs0320@gmail.com>
2021-03-04 15:25:27 +0900

    이미지 핀치

--- PinchGesture/PinchGesture/ViewController.swift
+++ PinchGesture/PinchGesture/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var Pinch: UILabel!
+//    @IBOutlet var Pinch: UILabel!
+    
+    @IBOutlet var imgPinch: UIImageView!
     
     var initialFontSize: CGFloat!
     
@@ -21,17 +23,25 @@ class ViewController: UIViewController
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
         self.view.addGestureRecognizer(pinch)
     }
+
     
+// 텍스트 핀치
+//    @objc func doPinch(_ pinch: UIPinchGestureRecognizer)
+//    {
+//        if (pinch.state == UIGestureRecognizer.State.began)
+//        {
+//            initialFontSize = Pinch.font.pointSize
+//        }
+//        else
+//        {
+//            Pinch.font = Pinch.font.withSize(initialFontSize * pinch.scale)
+//        }
+//    }
+
     @objc func doPinch(_ pinch: UIPinchGestureRecognizer)
     {
-        if (pinch.state == UIGestureRecognizer.State.began)
-        {
-            initialFontSize = Pinch.font.pointSize
-        }
-        else
-        {
-            Pinch.font = Pinch.font.withSize(initialFontSize * pinch.scale)
-        }
+        imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
     }
 
 
